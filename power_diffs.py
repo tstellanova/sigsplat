2e2bc393cfe7936d93fd87a63bb8d1039fa2ab89
Todd Stellanova <tstellanova@users.noreply.github.com>
2024-09-07 18:02:28 -0700

    use lazy file data loading for large files

--- power_diffs.py
+++ power_diffs.py
@@ -12,6 +12,8 @@ import numpy as np
 import matplotlib
 import argparse
 import os
+import logging
+
 import sigmf
 from matplotlib.ticker import StrMethodFormatter
 from scipy import ndimage
@@ -55,7 +57,7 @@ def safe_log(data):
 
 from scipy.ndimage import gaussian_filter1d
 
-def gaussian_decimate(data, num_out_buckets, sigma=3.0):
+def gaussian_decimate(data, num_out_buckets, sigma=1.0):
     # Step 1: Apply Gaussian filter
     filtered_data = gaussian_filter1d(data, sigma=sigma)
 
@@ -75,6 +77,11 @@ def nearest_power2(N):
 
     # Return 2^(a + 1)
     return 2**(a + 1)
+
+def grab_one_integration(obs_obj=None, int_idx=0):
+    obs_obj.read_data(t_start=int_idx, t_stop=int_idx+1)
+    _, cur_ps = obs_obj.grab_data()
+    return cur_ps
 
 def main():
 
@@ -96,15 +103,24 @@ def main():
 
     data_path = args.src_data_path
     print(f'Loading file info: {data_path} ...')
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", "DEBUG"))
+    loggers = [logging.getLogger()]  # get the root logger
+    loggers = loggers + [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+    print(f"Loggers: {loggers}")
+    # logging.getLogger("root").setLevel(logging.DEBUG)
+    logging.getLogger("blimpy.io.base_reader").setLevel(logging.DEBUG)
+    logging.getLogger("blimpy").setLevel(logging.DEBUG)
+
     perf_start = perf_counter()
-    obs_obj = blimpy.Waterfall(data_path)
-    print(f"elapsed: {perf_counter()  - perf_start} seconds")
+    obs_obj = blimpy.Waterfall(data_path, max_load=16, load_data=False)
+
+    print(f"elapsed: {perf_counter()  - perf_start:0.3f} seconds")
 
     # dump some interesting facts about this observation
     print(">>> Dump observation info ...")
     perf_start = perf_counter()
     obs_obj.info()
-    print(f"elapsed: {perf_counter() - perf_start} seconds")
+    print(f"elapsed: {perf_counter()  - perf_start:0.3f} seconds")
 
     if 'rawdatafile' in obs_obj.header:
         print(f"Source raw data file: {obs_obj.header['rawdatafile']}")
@@ -117,7 +133,7 @@ def main():
     print("Calculate coarse channels ...")
     perf_start = perf_counter()
     n_coarse_chan = int(obs_obj.calc_n_coarse_chan())
-    print(f"elapsed: {perf_counter() - perf_start} seconds")
+    print(f"elapsed: {perf_counter()  - perf_start:0.3f} seconds")
 
     n_fine_chan = obs_obj.header['nchans']
     fine_channel_bw_mhz = obs_obj.header['foff']
@@ -128,29 +144,38 @@ def main():
     else: # file data stores ascending frequency values
         start_freq = obs_obj.container.f_start
         stop_freq = obs_obj.container.f_stop - fine_channel_bw_mhz
-    print(f"fine_channel_bw_mhz: {fine_channel_bw_mhz} start: {start_freq} stop: {stop_freq}")
+    print(f"fine_channel_bw_mhz: {fine_channel_bw_mhz:0.6e} start: {start_freq:0.6f} stop: {stop_freq:0.6f}")
 
     fine_channel_bw_hz = np.abs(fine_channel_bw_mhz) * 1E6
-    print(f"n_coarse_chan: {n_coarse_chan} n_fine_chan: {n_fine_chan} fine_channel_bw_hz: {fine_channel_bw_hz:0.3f}")
+    print(f"n_coarse_chan: {n_coarse_chan} n_fine_chan: {n_fine_chan} fine_channel_bw_hz: {fine_channel_bw_hz:0.4f}")
     n_integrations_input = obs_obj.n_ints_in_file
     n_polarities_stored = obs_obj.header['nifs']
+    # We don't actually handle anything other than Stokes I at the moment
+    assert n_polarities_stored == 1
 
-    print(f"data shape: {obs_obj.data.shape} "
-          f"s/b: integrations {n_integrations_input}, polarities {n_polarities_stored}, n_fine_chan {n_fine_chan} ")
+    # if obs_obj.container.isheavy():
+    #     selection_size_bytes =  obs_obj.container._calc_selection_size()
+    #     selection_shape = obs_obj.container.selection_shape
+    #     data_array_size = obs_obj.container.max_data_array_size
+    #     print(f"heavy selection_shape: {selection_shape}  selection_size_bytes: {selection_size_bytes} data_array_size: {data_array_size}")
+
+    # print(f"data shape: {obs_obj.data.shape} "
+    #       f"s/b: integrations {n_integrations_input}, polarities {n_polarities_stored}, n_fine_chan {n_fine_chan} ")
 
     # validate that the input file data shape matches expectations set by header
-    assert n_integrations_input == obs_obj.data.shape[0]
-    assert n_polarities_stored == obs_obj.data.shape[1]
-    assert n_fine_chan == obs_obj.data.shape[2]
+    # # TODO this fails for large files
+    # assert n_integrations_input == obs_obj.data.shape[0]
+    # assert n_polarities_stored == obs_obj.data.shape[1]
+    # assert n_fine_chan == obs_obj.data.shape[2]
 
     n_integrations_to_process = n_integrations_input
-    if n_integrations_input > 1024:
-        n_integrations_to_process = 1024
+    if n_integrations_input > 256:
+        n_integrations_to_process = 256
         print(f"clamping n_integrations_to_process to {n_integrations_to_process}")
 
     # tsamp is "Time integration sampling rate in seconds" (from rawspec)
     integration_period_sec = float(obs_obj.header['tsamp'])
-    print(f"Integration period (tsamp): {integration_period_sec} seconds")
+    print(f"Integration period (tsamp): {integration_period_sec:0.6f} seconds")
     sampling_rate_mhz = np.abs(n_fine_chan * fine_channel_bw_mhz)
     print(f"Sampling bandwidth: {sampling_rate_mhz} MHz")
     sampling_rate_hz = sampling_rate_mhz * 1E6
@@ -162,45 +187,53 @@ def main():
     # n_fine_spectra_per_integration =
     # tsamp           cb_data[i].fb_hdr.tsamp = raw_hdr.tbin * ctx.Nts[i] * ctx.Nas[i]; // Time integration sampling rate in seconds.
 
-    one_sample = obs_obj.data[0,0,0]
     print(f"file nbits: {int(obs_obj.header['nbits'])}")
-    print(f"one_sample dtype: {np.dtype(one_sample)} iscomplex: {np.iscomplex(one_sample)}")
 
     power_diffs = np.zeros((n_integrations_to_process,n_fine_chan))
 
-    first_ps = obs_obj.data[0,0]
-    first_ps_normalized = normalize_power_spectrum(first_ps)
-    prior_ps_normalized = first_ps_normalized
+    print(f"Load first integration from disk...")
+    perf_start = perf_counter()
+    obs_obj.read_data(t_start=0, t_stop=1)
+    _, first_ps = obs_obj.grab_data(t_start=0, t_stop=1)
+    print(f"elapsed: {perf_counter()  - perf_start:0.3f} seconds")
+    print(f"one integration shape: {first_ps.shape} ")
+    prior_ps_normalized = normalize_power_spectrum(first_ps)
 
     print(f"Walking {n_integrations_to_process} integrations ...")
     perf_start = perf_counter()
     for int_idx in range(n_integrations_to_process):
-        cur_ps = obs_obj.data[int_idx,0]
+        print(f"integration: {int_idx}")
+        cur_ps = grab_one_integration(obs_obj,int_idx)
         cur_ps_normalized = normalize_power_spectrum(cur_ps)
-        power_diff = np.subtract(cur_ps_normalized, prior_ps_normalized) # x1 - x2
-        power_diffs[int_idx] = power_diff
+        cur_diff =  np.subtract(cur_ps_normalized, prior_ps_normalized) # x1 - x2
+        power_diffs[int_idx] = np.sign(cur_diff) * (cur_diff * cur_diff)
         prior_ps_normalized = cur_ps_normalized
+        cur_ps = None
 
-    print(f"elapsed: {perf_counter() - perf_start} seconds")
+    prior_ps_normalized = None
+    print(f"elapsed: {perf_counter()  - perf_start:0.3f} seconds")
 
     # we now have a huge array of num_integrations x num_fine_channels,
     # something like 16 x 1048576 difference values
     # need to decimate to make platting work
 
-    down_buckets =  2048
-    decimated_data = power_diffs
+    down_buckets =  1024
+
     if n_fine_chan > down_buckets:
-        print(f"Decimating from {n_fine_chan} fine channels to {down_buckets}, ratio: {n_fine_chan / down_buckets} ...")
+        print(f"Decimating {n_fine_chan} fine channels to {down_buckets} buckets, ratio: {n_fine_chan / down_buckets} ...")
         perf_start = perf_counter()
         # decimated_data = np.array([scipy.signal.decimate(row, down_buckets, ftype='iir') for row in power_diffs])
-        decimated_data = np.array([scipy.signal.decimate(row, down_buckets, ftype='fir') for row in power_diffs])
-        # decimated_data = np.array([gaussian_decimate(row, down_buckets) for row in power_diffs])
-        print(f"elapsed: {perf_counter() - perf_start:0.3f} seconds")
+        # decimated_data = np.array([scipy.signal.decimate(row, down_buckets, ftype='fir') for row in power_diffs])
+        decimated_data = np.array([gaussian_decimate(row, down_buckets) for row in power_diffs])
+        print(f"elapsed: {perf_counter()  - perf_start:0.3f} seconds")
     else:
         down_buckets = n_fine_chan
+        decimated_data = power_diffs
 
-    log_scaled_data = safe_log(decimated_data)
-    plot_data = log_scaled_data
+    power_diffs = None
+    # plot_data = decimated_data
+    plot_data = safe_log(decimated_data)
+    decimated_data = None
 
     print(f"Plotting {plot_data.shape} ...")
     perf_start = perf_counter()
@@ -209,7 +242,8 @@ def main():
     # my_dpi=300
     # full_screen_dims=(3024 / my_dpi, 1964 / my_dpi)
     full_screen_dims=(16, 10)
-    fig, axes = plt.subplots(nrows=2, figsize=full_screen_dims,  layout='constrained')
+    fig, axes = plt.subplots(nrows=2, figsize=full_screen_dims,  constrained_layout=True,  sharex=True)
+    fig.subplots_adjust(hspace=0)
     fig.suptitle(f"{start_freq:0.4f} | {src_data_filename}")
 
     img0 = axes[0].imshow(plot_data.T, aspect='auto', cmap='viridis')
@@ -233,7 +267,7 @@ def main():
     axes[0].set_ylabel('BB MHz')
     axes[1].set_ylabel('BB MHz')
 
-    print(f"elapsed: {perf_counter() - perf_start} seconds")
+    print(f"elapsed: {perf_counter()  - perf_start:0.3f} seconds")
     img_save_path = f"./img/{src_data_filename}.powdiff.png"
     print(f"saving image to:\n{img_save_path}")
     plt.savefig(img_save_path)
